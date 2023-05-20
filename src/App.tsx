5d1199933d9a708a8a2e9b9ce1c75d679f660972
angrylid <zm4399@gmail.com>
2023-05-20 17:32:24 +0800

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-export const App = () => {
-  return (
-    <h1>Hello World!</h1>
-  );
-};
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Index from "./pages";
+const router = createBrowserRouter([
+  {
+    element: <Index />,
+    path: "/",
+  },
+]);
+export const App = () => {
+  return <RouterProvider router={router} />;
+};
