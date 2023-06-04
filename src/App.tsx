a4c4fbc8043cc6cd7e48a2bd6238eb1c54db8c2f
angrylid <zm4399@gmail.com>
2023-06-04 18:18:59 +0800

    shadcn-ui init

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { router } from "./routes";
 
+import "./styles/globals.css";
 export const App = () => {
   return (
     <Provider store={store}>
